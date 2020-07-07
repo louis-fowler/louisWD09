0b2623e2bdc45381b31f8cc8ed26a2e9429a9541
Louis <louis.fowler@gmail.com>
2020-07-07 13:47:36 +1200

    test

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -7,7 +7,7 @@ export default function Contact() {
     <div style={{ color: `teal` }}>
       <Link to="/">Home</Link>
       <Header headerText="Contact" />
-      <p>Send us a message!</p>
+      <p>Send us a text!</p>
     </div>
   )
 }
